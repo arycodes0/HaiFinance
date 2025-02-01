031921a4a766ded32e23b9de7ce4687a908b42d5
arycodes0 <alvarezaryam@gmail.com>
2025-02-01 04:45:53 -0400

    Frontend: Home

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container mt-5">
+  <h1 className="text-black text-4xl font-bold">
+  Welcome to HaiBank!
+</h1>
+      <p className="lead">Your place to view and manage your accounts, credit cards, and loans easily.</p>
+    </div>
+  );
+}
+
+export default Home;
