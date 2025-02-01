6f797033fb30b8d120b0ca16df36d1aa4adb6549
arycodes0 <alvarezaryam@gmail.com>
2025-02-01 04:42:14 -0400

    Frontend: Components

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar">
+      <div className="container">
+        <Link className="navbar-brand" to="/">HaiBank Management</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
